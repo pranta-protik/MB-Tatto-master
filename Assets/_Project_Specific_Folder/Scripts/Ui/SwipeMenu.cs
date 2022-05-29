157a6c76df4f304145184ce94f91143d2b7d6384
Pranta Protik <pranta.cseku@gmail.com>
2022-05-29 23:02:52 +0600

    New card condition added

--- Assets/_Project_Specific_Folder/Scripts/Ui/SwipeMenu.cs
+++ Assets/_Project_Specific_Folder/Scripts/Ui/SwipeMenu.cs
@@ -91,6 +91,8 @@ public class SwipeMenu : MonoBehaviour
     {
         if (_selectedCard.requirementType == HandCard.ERequirementType.Cash)
         {
+            unlockInfoText.gameObject.SetActive(false);
+            
             if (PlayerPrefs.GetInt("HandCard" + handCard.handId) == 0)
             {
                 actionButton.gameObject.SetActive(true);
@@ -117,10 +119,31 @@ public class SwipeMenu : MonoBehaviour
 
         if (_selectedCard.requirementType == HandCard.ERequirementType.Time)
         {
-            if (PlayerPrefs.GetInt("HandCard" + handCard.handId) == 0)
-            {
-                
-            }
+            CheckUnlockTextRequirement(handCard, "Play game for " + handCard.requiredTime + "/" + handCard.requiredTime + " min Time");
+        }
+
+        if (_selectedCard.requirementType == HandCard.ERequirementType.GamePlay)
+        {
+            CheckUnlockTextRequirement(handCard, "Play the game " + handCard.requiredMatches + "/" + handCard.requiredMatches + " times");
+        }
+
+        if (_selectedCard.requirementType == HandCard.ERequirementType.Level)
+        {
+            CheckUnlockTextRequirement(handCard, "Unlock after reaching level " + handCard.requiredLevelNo);
+        }
+    }
+
+    private void CheckUnlockTextRequirement(HandCard handCard, string unlockText)
+    {
+        if (PlayerPrefs.GetInt("HandCard" + handCard.handId) == 0)
+        {
+            unlockInfoText.gameObject.SetActive(true);
+            unlockInfoText.SetText(unlockText);
+            startButton.interactable = false;
+        }
+        else
+        {
+            startButton.interactable = true;
         }
     }
 }
