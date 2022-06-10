ce129b658710c5a6d3435405a17fc7a5901e6516
Pranta Protik <pranta.cseku@gmail.com>
2022-06-10 22:20:28 +0600

    Influence meter fix

--- Assets/_Project_Specific_Folder/Scripts/InfluenceMeter.cs
+++ Assets/_Project_Specific_Folder/Scripts/InfluenceMeter.cs
@@ -1,16 +1,114 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
 public class InfluenceMeter : MonoBehaviour
 {
-    private TextMeshProUGUI _followersText;
+    public float playerIconMoveDistance;
+    public float playerIconMoveDuration;
+    public float playerIconBaseYValue;
+    public float playerIconMaxYValue;
+    
+    public float meterScrollDownThreshold;
+    public float meterScrollDownAmount;
+    public float meterScrollDownDuration;
+    public float meterIconBaseYValue;
 
+    private float _scrollDownThreshold;
+    private RectTransform _playerIcon;
+    private RectTransform _meterIcon;
+    private TextMeshProUGUI _followersText;
+    private TextMeshProUGUI _cashText;
+
+    private float _currentCash;
+    private float _startCash;
+    private int _targetCash;
+    private bool _shouldUpdateCashText;
+    
     private void Start()
     {
+        _cashText = transform.GetChild(2).GetComponent<TextMeshProUGUI>();
+        _cashText.SetText("$0");
+        
         _followersText = transform.GetChild(1).GetChild(0).GetChild(0).GetChild(1).GetComponent<TextMeshProUGUI>();
         _followersText.SetText(PlayerPrefs.GetInt("LastFollowers", 0).ToString());
+
+        _targetCash = StorageManager.Instance.RewardValue < 0 ? 500 : StorageManager.Instance.RewardValue;
+        _currentCash = 0;
+        _startCash = _currentCash;
+        _shouldUpdateCashText = true;
+    }
+
+    private void Update()
+    {
+        if (_shouldUpdateCashText)
+        {
+            UpdateCashText();
+        }
+    }
+
+    private void UpdateInfluenceMeter()
+    {
+        _scrollDownThreshold = PlayerPrefs.GetFloat("ScrollDownThreshold", meterScrollDownThreshold);
+        
+        _playerIcon = transform.GetChild(1).GetChild(0).GetChild(0).GetComponent<RectTransform>();
+        _playerIcon.anchoredPosition = new Vector2(110f, PlayerPrefs.GetFloat("PlayerIconYValue", playerIconBaseYValue));
+        
+        _meterIcon = transform.GetChild(1).GetChild(0).GetComponent<RectTransform>();
+        _meterIcon.anchoredPosition = new Vector2(0f, PlayerPrefs.GetFloat("MeterIconYValue", meterIconBaseYValue));
+
+        if (_playerIcon.anchoredPosition.y < playerIconMaxYValue)
+        {
+            if (_playerIcon.anchoredPosition.y >= _scrollDownThreshold)
+            {
+                _meterIcon.DOAnchorPosY(_meterIcon.anchoredPosition.y - meterScrollDownAmount, meterScrollDownDuration).OnComplete(() =>
+                {
+                    PlayerPrefs.SetFloat("MeterIconYValue", _meterIcon.anchoredPosition.y);
+                
+                    _scrollDownThreshold += meterScrollDownThreshold;
+                    PlayerPrefs.SetFloat("ScrollDownThreshold", _scrollDownThreshold);
+                
+                    UpdatePlayerIcon();
+                });
+            }
+            else
+            {
+                UpdatePlayerIcon();
+            }   
+        }
+    }
+
+    private void UpdatePlayerIcon()
+    {
+        _playerIcon.DOAnchorPosY(_playerIcon.anchoredPosition.y + playerIconMoveDistance, playerIconMoveDuration).OnComplete(() =>
+        {
+            PlayerPrefs.SetFloat("PlayerIconYValue", _playerIcon.anchoredPosition.y);
+            Invoke(nameof(EnableUnlockScreen), 0.5f);
+        });   
+    }
+
+    private void EnableUnlockScreen()
+    {
+        UiManager.Instance.UnlockPanel.SetActive(true);
+        gameObject.SetActive(false);
+    }
+    
+    private void UpdateCashText()
+    {
+        if (_currentCash < _targetCash)
+        {
+            _currentCash += ((_targetCash - _startCash) / 2f) * Time.deltaTime;
+            _currentCash = Mathf.Clamp(_currentCash, 0, _targetCash);
+        }
+        else
+        {
+            _shouldUpdateCashText = false;
+            StorageManager.SaveTotalCoin(StorageManager.GetTotalCoin() + _targetCash);
+            UpdateInfluenceMeter();
+        }
+        _cashText.SetText("$" + Mathf.RoundToInt(_currentCash));
     }
 }
