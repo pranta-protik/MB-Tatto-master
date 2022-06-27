29fb7e43f60251eba4203aea4a17f9c61aa4e6fa
Pranta Protik <pranta.cseku@gmail.com>
2022-06-27 11:22:49 +0600

    Filter buttons fix

---
+++ Assets/_Project_Specific_Folder/Scripts/FilterManager.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+public class FilterManager : MonoBehaviour
+{
+    [SerializeField] private MobileScreen mobileScreen;
+    public int currentFilterButtonId;
+    private GameObject _captureButton;
+    private GameObject _watchAdButton;
+
+    private void Start()
+    {
+        _captureButton = mobileScreen.transform.GetChild(3).gameObject;
+        _watchAdButton = mobileScreen.transform.GetChild(4).gameObject;
+    }
+
+    public void FilterEffect1(GameObject filterButtonObj)
+    {
+        NormalFilterEffect(filterButtonObj);
+    }
+
+    public void FilterEffect2(GameObject filterButtonObj)
+    {
+        NormalFilterEffect(filterButtonObj);
+    }
+
+    public void FilterEffect3(GameObject filterButtonObj)
+    {
+        WatchAdFilterEffect(filterButtonObj);
+    }
+    
+    public void FilterEffect4(GameObject filterButtonObj)
+    {
+        WatchAdFilterEffect(filterButtonObj);
+    }
+    
+    public void FilterEffect5(GameObject filterButtonObj)
+    {
+        WatchAdFilterEffect(filterButtonObj);
+    }
+    
+    private void NormalFilterEffect(GameObject currentFilterObj)
+    {
+        SetCurrentFilterId(currentFilterObj);
+        EnableCaptureButton();
+    }
+
+    private void WatchAdFilterEffect(GameObject currentFilterObj)
+    {
+        SetCurrentFilterId(currentFilterObj);
+        
+        if (PlayerPrefs.GetInt("AdWatched" + currentFilterButtonId, 0) == 0)
+        {
+            EnableWatchAdButton();
+        }
+        else
+        {
+            EnableCaptureButton();
+        }
+    }
+    
+    private void SetCurrentFilterId(GameObject currentFilterObj)
+    {
+        FilterButton filterButton = currentFilterObj.GetComponent<FilterButton>();
+        currentFilterButtonId = filterButton.buttonId;
+    }
+    
+    private void EnableCaptureButton()
+    {
+        _captureButton.SetActive(true);
+        _watchAdButton.SetActive(false);    
+    }
+
+    private void EnableWatchAdButton()
+    {
+        _captureButton.SetActive(false);
+        _watchAdButton.SetActive(true);
+    }
+}
--- Assets/_Project_Specific_Folder/Scripts/PostProcessHandler.cs
+++ Assets/_Project_Specific_Folder/Scripts/PostProcessHandler.cs
@@ -3,40 +3,34 @@ using UnityEngine.Rendering.PostProcessing;
 
 public class PostProcessHandler : MonoBehaviour
 {
-    [SerializeField] private PostProcessProfile _postProcessProfile;
-    
-    private bool _isGrayscale;
-    
+    private PostProcessProfile _postProcessProfile;
+
     private void Start()
     {
         _postProcessProfile = GetComponent<PostProcessVolume>().profile;
     }
 
-    public void AddGrayscaleEffect()
+    public void AddGrayscaleFilter()
     {
-        if (_isGrayscale)
-        {
-            _isGrayscale = false;
-            RemoveGrayscaleEffect();
-        }
-        else
-        {
-            _isGrayscale = true;
-            _postProcessProfile.AddSettings<ColorGrading>();
-            ColorGrading colorGrading = _postProcessProfile.GetSetting<ColorGrading>();
+        RemoveAllFilters();
         
-            // Grading Mode
-            colorGrading.gradingMode.overrideState = true;
-            colorGrading.gradingMode.value = GradingMode.LowDefinitionRange;
+        _postProcessProfile.AddSettings<ColorGrading>();
+        ColorGrading colorGrading = _postProcessProfile.GetSetting<ColorGrading>();
         
-            // Saturation
-            colorGrading.saturation.overrideState = true;
-            colorGrading.saturation.value = -100;
-        }
+        // Grading Mode
+        colorGrading.gradingMode.overrideState = true;
+        colorGrading.gradingMode.value = GradingMode.LowDefinitionRange;
+        
+        // Saturation
+        colorGrading.saturation.overrideState = true;
+        colorGrading.saturation.value = -100;
     }
 
-    private void RemoveGrayscaleEffect()
+    public void RemoveAllFilters()
     {
-        _postProcessProfile.RemoveSettings<ColorGrading>();
+        if (_postProcessProfile.HasSettings<ColorGrading>())
+        {
+            _postProcessProfile.RemoveSettings<ColorGrading>();   
+        }
     }
 }
