b5c6e1b3f4c72c09d6ff722235e1389401dec207
svpinto <67506210+svpinto@users.noreply.github.com>
2022-07-05 15:39:47 +0400

    Added googleservice info for IOS

--- Assets/Homa Games/Homa Belly/Core/Scripts/HomaBridge/Dependencies/HomaBridgeDependenciesAttributions.cs
+++ Assets/Homa Games/Homa Belly/Core/Scripts/HomaBridge/Dependencies/HomaBridgeDependenciesAttributions.cs
@@ -7,6 +7,8 @@ namespace HomaGames.HomaBelly
 // Homa Belly services won't run on Unity Editor. This is a design decision to allow some runtime optimizations
 #if !UNITY_EDITOR
 			 // This method will be filled automatically by:HomaBridgeDependenciesCodeGenerator when Homa Belly services are added/removed to the project.
+			 attributions.Add(new AdjustAttributionAdapter());
+			 attributions.Add(new SingularAttribution());
 #endif
 	 	 }
 	 }
