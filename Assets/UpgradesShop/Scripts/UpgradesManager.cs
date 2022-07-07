4beb6a3257b61f5c47156a2a89f1db1edb5e24b5
InigoKenyon91 <inigo.cabrejas@homagames.com>
2022-07-07 17:06:23 +0200

    Get methods for easy acces

--- Assets/UpgradesShop/Scripts/UpgradesManager.cs
+++ Assets/UpgradesShop/Scripts/UpgradesManager.cs
@@ -9,25 +9,45 @@ public class UpgradesManager : Performance_Singleton<UpgradesManager>
     [SerializeField] private string sceneName;
 
     public MachineUpgradeSO MachineUpgradeSo => upgradesModel.machineUpgrade;
-    public TattooUpgradeSO SelectedTattooUpgrade => upgradesModel.selectedTattooUpgrade; 
+    public TattooUpgradeSO SelectedTattooUpgrade => upgradesModel.selectedTattooUpgrade;
     public JewelryUpgradeSO SelectedJewelryUpgrade => upgradesModel.selectedJewelryUpgrade;
 
     protected override void Awake()
     {
         base.Awake();
-        
+
         exitShopTrigger.ExitShopAction += OnExitShop;
     }
 
     protected override void OnDestroy()
     {
         base.OnDestroy();
-        
+
         exitShopTrigger.ExitShopAction += OnExitShop;
     }
 
     private void OnExitShop()
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    public GameObject GetTatgun()
+    {
+        return MachineUpgradeSo.GetMachine();
+    }
+
+    public Color GetInkColor()
+    {
+        return MachineUpgradeSo.GetInkColor();
+    }
+
+    public Sprite GetTattoo()
+    {
+        return SelectedTattooUpgrade.GetTattoo();
+    }
+
+    public GameObject GetJewel()
+    {
+        return SelectedJewelryUpgrade.GetJewelry();
     }
 }
