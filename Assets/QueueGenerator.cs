a760baa97e58782e3f153130774b61958c974394
Redwan Islam <wahedifad@gmail.com>
2022-07-14 01:53:31 +0600

    shop setup

---
+++ Assets/QueueGenerator.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QueueGenerator : MonoBehaviour
+{
+    public List<Transform> Points;
+
+    public GameObject CustomerPrefab;
+
+    GameObject _customerPrefab;
+    public List<GameObject> Customers;
+    void Awake()
+    {
+        for (int i = 0; i < Points.Count; i++) {
+          _customerPrefab =  Instantiate(CustomerPrefab, Points[i].transform.position, Quaternion.identity);
+          Customers.Add(_customerPrefab);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Reception.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class Reception : MonoBehaviour
+{
+    public QueueGenerator queueGenerator;
+    public Transform StandPos;
+
+    public GameObject CurrentPassenger;
+    bool a;
+    void Start()
+    {
+      
+        queueGenerator.Customers[0].transform.DOMove(StandPos.transform.position, 0);
+        CurrentPassenger = queueGenerator.Customers[0].gameObject;
+        queueGenerator.Customers.RemoveAt(0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(CurrentPassenger == null)
+        {
+
+            queueGenerator.Customers[0].transform.DOMove(StandPos.transform.position, 2);
+            CurrentPassenger = queueGenerator.Customers[0].gameObject;
+            queueGenerator.Customers.RemoveAt(0);
+        }
+    }
+}
