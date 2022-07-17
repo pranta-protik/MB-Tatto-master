19db3dce9f4487dd6ec2551d5ae08bcedabda31f
Pranta Protik <pranta.cseku@gmail.com>
2022-07-17 20:20:03 +0600

    Transition screen added

---
+++ Assets/_UpgradesShop/Scripts/TransitionScreen.cs
@@ -0,0 +1,18 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TransitionScreen : MonoBehaviour
+{
+    private Image transitionImage;
+    
+    private void Start()
+    {
+        transitionImage = GetComponent<Image>();
+
+        transitionImage.DOFade(0, 1f).OnComplete(() =>
+        {
+            transitionImage.gameObject.SetActive(false);
+        });
+    }
+}
